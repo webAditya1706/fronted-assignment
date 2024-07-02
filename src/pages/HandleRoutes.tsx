c457d781a6248b7fb457ca9b0efbe24d7bbf8c17
next developer <adityawebiwork@gmail.com>
2024-07-02 21:47:28 +0530

    add routing

--- src/pages/HandleRoutes.tsx
+++ src/pages/HandleRoutes.tsx
@@ -9,21 +9,21 @@ const HandleRoute = () => {
     const token = typeof window !== 'undefined' ? localStorage.getItem("assignToken") : null;
 
     useEffect(() => {
-        if (!token || !loginUserData || !Object.keys(loginUserData).length) {
-            if (router.route !== "/signin") {
-                router.push("/signin"); // Redirect to signin page if no token or user data
-            }
-        } else {
-            if (loginUserData.role === "admin" && AdminRoutes.includes(router.route)) {
-                router.push("/");
-            }
-            if (loginUserData.role === "user" && UserRoutes.includes(router.route) ) {
-                router.push("/");
-            }
-            if (SupperAdminRoutes.includes(router.route) && loginUserData.role !== "superAdmin") { // Corrected spelling
-                router.push("/");
-            }
-        }
+        // if (!token || !loginUserData || !Object.keys(loginUserData).length) {
+        //     if (router.route !== "/signin") {
+        //         router.push("/signin"); // Redirect to signin page if no token or user data
+        //     }
+        // } else {
+        //     if (loginUserData.role === "admin" && AdminRoutes.includes(router.route)) {
+        //         router.push("/");
+        //     }
+        //     if (loginUserData.role === "user" && UserRoutes.includes(router.route) ) {
+        //         router.push("/");
+        //     }
+        //     if (SupperAdminRoutes.includes(router.route) && loginUserData.role !== "superAdmin") { // Corrected spelling
+        //         router.push("/");
+        //     }
+        // }
     }, [loginUserData, token, router.route]);
 
     return null;
